34e318621decf6bdec9f8bf8c03097aac2310b0c
leandrodalbo <leandro.edbo@gmail.com>
2025-05-13 13:22:00 -0300

    project ready to work

---
+++ src/main/kotlin/com/ticketera/auth/TicketeraAuthApplication.kt
@@ -0,0 +1,11 @@
+package com.ticketera.auth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TicketeraAuthApplication
+
+fun main(args: Array<String>) {
+	runApplication<TicketeraAuthApplication>(*args)
+}
