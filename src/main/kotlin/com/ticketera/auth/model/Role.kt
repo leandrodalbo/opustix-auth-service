15f66991ca82d623932b12ab3a6744e2ab74441e
leandrodalbo <leandro.edbo@gmail.com>
2025-05-14 14:05:32 -0300

    Day 2: Define the Domain Model

---
+++ src/main/kotlin/com/ticketera/auth/model/Role.kt
@@ -0,0 +1,5 @@
+package com.ticketera.auth.model
+
+enum class Role {
+    ADMIN, USER
+}
---
+++ src/test/kotlin/com/ticketera/auth/repository/UserRepositoryTest.kt
@@ -0,0 +1,55 @@
+package com.ticketera.auth.repository
+
+import com.ticketera.auth.model.AuthProvider
+import com.ticketera.auth.model.Role
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(
+    replace = AutoConfigureTestDatabase.Replace.NONE
+)
+@Testcontainers
+class UserRepositoryTest {
+    companion object {
+        @Container
+        val postgresContainer = PostgreSQLContainer("postgres:16.4").apply {
+            withDatabaseName("testdb")
+            withUsername("test")
+            withPassword("test")
+        }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun configureProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgresContainer::getJdbcUrl)
+            registry.add("spring.datasource.username", postgresContainer::getUsername)
+            registry.add("spring.datasource.password", postgresContainer::getPassword)
+        }
+    }
+
+    @Autowired
+    private val repository: UserRepository? = null
+
+
+    @Test
+    fun shouldAllUsers() {
+        val user = repository?.findAll()?.get(0)
+
+        assertThat(user?.id).isNotNull()
+        assertThat(user?.email).isNotNull()
+        assertThat(user?.password).isNotNull()
+        assertThat(user?.roles).contains(Role.USER)
+        assertThat(user?.authProvider).isEqualTo(AuthProvider.LOCAL)
+
+    }
+}
